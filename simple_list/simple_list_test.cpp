7f59dd55df0705fbd547b5b645490b07344a876f
sean.lee <sean@seanlees-MacBook-Pro.local>
2022-08-19 16:33:16 +0900

    simple list update

--- simple_list/simple_list_test.cpp
+++ simple_list/simple_list_test.cpp
@@ -6,7 +6,7 @@ typedef float TYPE;
 
 
 /* 수정사항
- * true , false 로 return 하도록  
+ * true , false 로 return 하도록 -완료
  */
 
 int main()
@@ -14,6 +14,8 @@ int main()
     SimpleList<TYPE> slist; 
     char ch;
     TYPE data;
+
+    int test;
 
     for(int i=0;i<slist.getMaxSize()+2; i++){
         slist.push(i*1.0);
