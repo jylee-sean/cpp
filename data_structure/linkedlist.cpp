ff500e29eedf8aa4a2132e0ab4be1076d65fca11
mac <mac@macui-MacBookPro.local>
2022-08-22 22:43:51 +0900

    data structure directory added

---
+++ data_structure/linkedlist.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+
+typedef struct node{
+
+    int data;
+    node* next;
+
+};
+
+
+
+
+void printNode(node &n)
+{
+
+}
+int main(void)
+{
+    
+    node head;
+
+    node tail;
+
+
+
+    head.data = 1;
+    head.next = &tail;
+
+
+
+
+
+    return 0;
+}
