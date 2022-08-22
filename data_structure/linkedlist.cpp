19d715a6fa09cb337d6dea2bd2134047ce2585e0
mac <mac@macui-MacBookPro.local>
2022-08-23 04:40:58 +0900

    linkedlist update

--- data_structure/linkedlist.cpp
+++ data_structure/linkedlist.cpp
@@ -1,3 +1,7 @@
+/*
+ * https://www.youtube.com/watch?v=AUY_wbHAq14&list=PLDV-cCQnUlIbV0z51Mwbbe-tdW2JDS-bR
+ */
+
 #include <iostream>
 
 
@@ -9,24 +13,56 @@ typedef struct node{
 };
 
 
+class LinkedList{
 
 
-void printNode(node &n)
-{
+    private:
 
-}
+
+    public:
+        LinkedList(){
+            node* head = NULL;
+            node* tail = NULL;
+        }
+        void addNode(int val){
+            node* newNode = new node;
+
+            newNode->data = val;
+            newNode->next = NULL;
+
+
+        }
+
+        void display(){
+
+        }
+
+        void getHead(){
+
+        }
+
+        void addFrontNode(int val){
+
+        }
+
+        void insertNode(node *pNode, int val){
+
+        }
+
+        void delteNode(node *pNode){
+
+        }
+};
+
+
 int main(void)
 {
     
-    node head;
+    LinkedList llist;
 
-    node tail;
-
-
-
-    head.data = 1;
-    head.next = &tail;
-
+    llist.addNode(1);
+    llist.addNode(2);
+    llist.addNode(3);
 
 
 
